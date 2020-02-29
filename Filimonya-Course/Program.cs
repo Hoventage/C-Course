c3b24664d78a495c15c19d6366ec87df4aa729c5
Филимонов Илья Юрьевич <Filimonov.IYu@esphere.local>
2020-02-29 20:35:27 +0300

    Add project files.

---
+++ Filimonya-Course/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+
+namespace Filimonov_Course
+{
+    class Draft
+    {
+        static void Main(string[] args)
+        {
+            #region
+
+
+
+
+            #endregion
+
+            #region Boolean (и работа с операторами)
+
+            //bool trueOrFalse = 10 < 555;
+            //bool testForFun = "гном" != "дворф";
+
+            //Console.WriteLine(trueOrFalse);
+            //Console.WriteLine(testForFun);
+
+            //bool flip = true;
+
+
+            //Console.WriteLine($"flip = {flip} !flip = {!flip} ");
+
+            //flip = false;
+
+            //Console.WriteLine($"flip = {flip} !flip = {!flip} ");
+
+
+            #endregion
+
+            #region Сложение двух int (с конвертацией string в int)
+
+            //Console.Write("Введите первое число: ");
+
+            //int int1 = Convert.ToInt32(Console.ReadLine());
+
+            //Console.Write("Введите второе число: ");
+
+            //int int2 = Convert.ToInt32(Console.ReadLine());
+
+            //Console.WriteLine(int1 + int2);
+
+            #endregion
+
+            #region Последовательный вывод нескольких переменных (склеивание строки)
+            var name1 = "123";
+            var name2 = "456";
+            var name3 = "789";
+
+            //Console.WriteLine("{0}, {1}, {2}!", name1, name2, name3);
+            //Console.WriteLine($"{name1}, {name2}, {name3}!");
+            //Console.Write("Введите чужое (не Ваше) имя: ");
+            //var notMyName = Console.ReadLine();
+            //Console.WriteLine(notMyName + "- Масянин гном :)");
+            Console.ReadKey();
+            #endregion
+        }
+    }
+}
