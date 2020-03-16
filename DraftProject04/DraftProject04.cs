16bb57875897c68318539a538ad5923ebe43be3e
Hoventage <ilya.filimonov.88@bk.ru>
2020-03-17 02:45:33 +0300

    Урок 4. Задание 1 и 4.

---
+++ DraftProject04/DraftProject04.cs
@@ -0,0 +1,156 @@
+﻿using System;
+using System.IO;
+
+namespace DraftProject04
+{
+    class DraftProject04
+    {
+
+        struct Man
+        {
+            public Man(string Name, int Age, int Salary, int[] ArrayName)
+            {
+                this.Name = Name;
+                this.Age = Age;
+                this.Salary = Salary;
+
+                testArray = ArrayName;
+            }
+
+            public int Age { get; set; }
+            public int Salary { get; set; }
+            public string Name { get; set; }
+
+            int[] testArray;
+
+            public override string ToString()
+            {
+                return $"{Name} {Age} {Salary}";
+            }
+        }
+
+
+
+        //struct Ppls
+        //{
+        //    public int Age { get; set; }
+        //    public int Salary { get; set; }
+        //    public string Name { get; set; }
+
+        //    public override string ToString()
+        //    {
+        //        return $"{Name} {Age} {Salary}";
+        //    }
+        //}
+
+
+        static void Main(string[] args)
+        {
+            #region Example 1
+            ////Пример метода, полностью отвязанного от потенциального интерфейса
+
+            //int WhatIsMax(int a, int b)
+            //{   
+            //    // is this true or not? yes : no
+            //    return a > b ? a : b;
+            //}
+
+            //Console.WriteLine(WhatIsMax(-4, 11));
+            //Console.ReadKey();
+            #endregion
+
+            //Arrays
+
+            #region Array1
+            //Ppls[] people = new Ppls[7]
+            //{
+            //    new Ppls() { Age = 10, Salary = 1000, Name = "Andrew1" },
+            //    new Ppls() { Age = 10, Salary = 1000, Name = "Andrew2" },
+            //    new Ppls() { Age = 10, Salary = 1000, Name = "Andrew3" },
+            //    new Ppls() { Age = 10, Salary = 1000, Name = "Andrew4" },
+            //    new Ppls() { Age = 10, Salary = 1000, Name = "Andrew5" },
+            //    new Ppls() { Age = 10, Salary = 1000, Name = "Andrew6" },
+            //    new Ppls() { Age = 10, Salary = 1000, Name = "Andrew7" }
+            //};
+
+            //Console.WriteLine(people[0].ToString());
+
+            #endregion
+
+            #region Array2
+            //Ppls[] people2 = new Ppls[2];
+
+            //people2[0] = new Ppls { Age = 10, Name = "qwerty7", Salary = 1000 };
+            //people2[1] = new Ppls { Age = 10, Name = "qwerty7", Salary = 1000 };
+            //people2[2] = new Ppls { Age = 10, Name = "qwerty7", Salary = 1000 };
+            #endregion
+
+            #region Array3
+            //int[] newArray = { 9, 8, 7, 6, 5, 4, 3, 2, 1, 0 };
+
+            //foreach (var item in newArray) Console.Write($"{item} "); Console.WriteLine();
+
+            //Array.Sort(newArray);
+
+            //foreach (var item in newArray) Console.Write($"{item} "); Console.WriteLine();
+            #endregion
+
+            #region Array4
+
+            //string[,] kindaMatrix = new string[4, 3]
+            //{
+            //    {"00", "01", "02" },
+            //    {"10", "11", "12" },
+            //    {"20", "21", "22" },
+            //    {"30", "31", "32" }
+            //};
+
+            //for (int i = 0; i < 4; i++)
+            //{
+            //    for (int q = 0; q < 3; q++)
+            //    {
+            //        Console.Write($"{kindaMatrix[i,q]} ");
+            //    }
+            //    Console.WriteLine();
+            //}
+
+            #endregion
+
+            #region Array5
+
+            //int[,] pascal = new int[10, 10];
+
+            //for (int i = 0; i < pascal.GetLength(0); i++)
+            //{
+            //    pascal[i, 0] = 1;
+            //    pascal[i, i] = 1;
+            //};
+
+            //for (int i = 0; i < pascal.GetLength(0); i++)
+            //{
+            //    for (int j = 0; j < pascal.GetLength(1); j++)
+            //    {
+            //        if (j <= i) Console.Write($"{pascal[i,j],5}");
+            //    }
+            //    Console.WriteLine();
+            //}
+
+
+            #endregion
+
+            #region Пример работы с файлом
+
+            string[] allLines =  File.ReadAllLines("TestFile.txt");
+
+            for (int i = 0; i < allLines.Length; i++)
+            {
+                int q = Convert.ToInt32(allLines[i]);
+                Console.WriteLine(q*10);
+            }
+
+            #endregion
+
+        }
+
+    }
+}
---
+++ LoginPasswordExtended/LoginPasswordExtended.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.IO;
+
+namespace LoginPasswordExtended
+{
+    /// <summary>
+    /// Задание 4.4. Решить задачу с логинами из урока 2, только логины и пароли считать из файла в массив.
+    /// Создайте структуру Account, содержащую Login и Password.
+    /// </summary>
+
+    class LoginPasswordExtended
+    {
+        struct Account
+        {
+            public Account(string Login, string Password)
+            {
+                this.Login = Login;
+                this.Password = Password;
+            }
+            public string Login { get; set; }
+            public string Password { get; set; }
+
+            public override string ToString()
+            {
+                return $"{Login} {Password}";
+            }
+        }
+        static void Main(string[] args)
+        {
+            // Записываем данные из файла в массив
+            string[] allLines = File.ReadAllLines("LoginPasswordExamples.txt");
+
+            // Выводим массив в консоли
+            for (int i = 0; i < allLines.Length; i++)
+            {
+                Console.WriteLine($"{allLines[i]}");
+            }
+
+            // Создаем массив, используя структуру Account. Кладем в него три наших аккаунта (в файле три пары логин-пароль)
+            Account[] array = new Account[]
+            {
+                new Account(allLines[0], allLines[1]),
+                new Account(allLines[2], allLines[3]),
+                new Account(allLines[4], allLines[5])
+            };
+
+            int n = 0;
+
+            // Перебираем пары логин-пароль, пока не наткнемся на нужную нам
+            do
+            {
+                Console.WriteLine($"Попытка авторизации № {n + 1}");
+
+                Console.WriteLine($"Ваши логин и пароль: {array[0 + n]}");
+
+                n++;
+
+            } while (!AuthorizeOrNot(array[0 + (n - 1)]));
+
+            // Немного измененный метод. Теперь на вход мы передаем объект типа Account
+            static bool AuthorizeOrNot(Account account)
+            {
+                if (account.Login == "root" && account.Password == "GeekBrains")
+                {
+                    Console.WriteLine("Авторизационные данные выбраны верно!");
+                    return true;
+                }
+                else
+                {
+                    Console.WriteLine("Авторизационные данные выбраны неверно :(");
+                    return false;
+                }
+            }
+            Console.ReadKey();
+        }
+    }
+}
